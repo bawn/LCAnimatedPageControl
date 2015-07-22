db8dd1d2748a126955bac87d7403cf61e4d68d0a
凌晨 <lc5491137@gmail.com>
2015-07-22 19:37:54 +0800

    update

--- Example/LCAnimatedPageControl/ViewController.m
+++ Example/LCAnimatedPageControl/ViewController.m
@@ -29,9 +29,10 @@
     self.pageControl = [[LCAnimatedPageControl alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 40, 280, 20)];
     self.pageControl.center = CGPointMake(self.view.frame.size.width * 0.5f, _pageControl.center.y);
     self.pageControl.numberOfPages = _number;
-    self.pageControl.indicatorDiameter = 5.0f;
+    self.pageControl.indicatorDiameter = 8.0f;
     self.pageControl.indicatorMargin = 20.0f;
-    self.pageControl.pageStyle = SquirmPageStyle;
+    self.pageControl.indicatorMultiple = 1.6;
+    self.pageControl.pageStyle = DepthColorPageStyle;
     self.pageControl.pageIndicatorColor = [UIColor colorWithRed:176.0f/255.0f green:176.0f/255.0f blue:176.0f/255.0f alpha:1.0f];
     self.pageControl.currentPageIndicatorColor = [UIColor colorWithRed:221.0f/255.0f green:34.0f/255.0f blue:56.0f/255.0f alpha:1.0f];
     self.pageControl.sourceScrollView = _collectionView;
@@ -47,7 +48,7 @@
 //    self.pageControl.indicatorDiameter = 5.0f;// Required
 //    self.pageControl.indicatorMargin = 18.0f;
 //    self.pageControl.indicatorMultiple = 1.8f;
-//    self.pageControl.pageStyle = ScalePageStyle;
+//    self.pageControl.pageStyle = ScaleColorPageStyle;
 //    self.pageControl.pageIndicatorColor = [UIColor colorWithRed:176.0f/255.0f green:176.0f/255.0f blue:176.0f/255.0f alpha:1.0f];
 //    self.pageControl.currentPageIndicatorColor = [UIColor colorWithRed:221.0f/255.0f green:34.0f/255.0f blue:56.0f/255.0f alpha:1.0f];
 //    self.pageControl.sourceScrollView = _collectionView;
@@ -60,7 +61,7 @@
 //                                [NSLayoutConstraint constraintWithItem:self.pageControl attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1.0f constant:0.0f],
 //                                [NSLayoutConstraint constraintWithItem:self.pageControl attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailing multiplier:1.0f constant:0.0f],
 //                                ]];
-    
+//    
 //    [self.collectionView setContentOffset:CGPointMake(self.collectionView.bounds.size.width, 0)];
     
     self.flowLayout.itemSize = CGSizeMake(self.collectionView.frame.size.width - 60, self.collectionView.frame.size.height - 100);
@@ -86,6 +87,7 @@
 }
 
 - (void)valueChanged:(LCAnimatedPageControl *)sender{
+    NSLog(@"%ld", (long)sender.currentPage);
     [self.collectionView setContentOffset:CGPointMake(self.collectionView.frame.size.width * sender.currentPage , self.collectionView.contentOffset.y) animated:YES];
 }
 
